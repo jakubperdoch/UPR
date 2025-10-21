42f8bb95215cad8e4a27fe791623b10cdd3972a9
Jakub Perdoch <perdochjakub@gmail.com>
2025-10-22 00:18:10 +0200

    Add turtle management functionality and improve array printing

--- Zadanie_5/main.c
+++ Zadanie_5/main.c
@@ -1,41 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct Turtle
 {
-    const int id;
     int row;
     int column;
-    int angle;
+    int direction;
 };
 
-void print_array(const char* array, const int* rows, const int* columns)
+void array_print(const char* array, const int rows, const int columns)
 {
-    for (int i = 0; i < *rows; i++)
+    for (int row = 0; row < rows; row++)
     {
-        for (int j = 0; j < *columns; j++)
+        for (int column = 0; column < columns; column++)
         {
-            printf("%c", array[i * (*columns) + j]);
+            printf("%c", array[row * columns + column]);
         }
         printf("\n");
     }
 }
 
+
+void add_turtle(struct Turtle** current_turtles, int* num_turtles)
+{
+    *num_turtles += 1;
+    struct Turtle* turtles = realloc(*current_turtles, *num_turtles * sizeof(struct Turtle));
+    struct Turtle t = {0, 0, 1};
+    turtles[*num_turtles - 1] = t;
+    *current_turtles = turtles;
+}
+
+void move_turtle(struct Turtle** current_turtles, int num_turtles)
+{
+    for (int i = 0; i < num_turtles; i++)
+    {
+        struct Turtle* turtle = current_turtles[i];
+        if (turtle->direction == 1)
+        {
+        }
+    }
+}
+
+
 int main(void)
 {
     int rows = 0, cols = 0;
+    struct Turtle* turtles = NULL;
+    int num_turtles = 0;
     scanf("%d %d", &rows, &cols);
 
     char* array = malloc(rows * cols * sizeof(char));
+    add_turtle(&turtles, &num_turtles);
 
     for (int i = 0; i < rows * cols; i++)
     {
         array[i] = '.';
     }
 
-    print_array(array, &rows, &cols);
+    array_print(array, rows, cols);
+
+    while (true)
+    {
+        char command;
+        scanf("%c", &command);
+
+        if (command == 'f' && num_turtles < 3)
+        {
+            add_turtle(&turtles, &num_turtles);
+            array_print(array, rows, cols);
+        }
+        else if (command == 'x')
+        {
+            break;
+        }
+    }
+
+    array_print(array, rows, cols);
+
 
     free(array);
+    free(turtles);
 
     return 0;
 }
